08ef45f8eeb00456df6f3b8234885125888ccdad
srowal <107223654+srowal@users.noreply.github.com>
2024-08-02 18:58:55 -0400

    fix stylesheets

--- html/index.php
+++ html/index.php
@@ -1,7 +1,11 @@
-<?php
-require_once './config.php';
-include "./include/header.php";
+<?php include "./include/header.php";
+define('BASE_URL', $_SERVER['SERVER_NAME'] . "/");
 ?>
+
+<link rel="stylesheet" href="./css/style.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
 <section id="queries-list">
 
